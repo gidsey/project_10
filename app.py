ccb520ae41f047fef2470793f3dbf0c52e4a2812
chrisguy <chris@chrisguy.co>
2019-12-05 16:49:50 +0000

    User model added

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, jsonify
 
 import config
 import models
+from auth import auth
 
+from resources.users import users_api
 from resources.todos import todos_api
 
+
 app = Flask(__name__)
+app.register_blueprint(users_api, url_prefix='/api/v1')
 app.register_blueprint(todos_api, url_prefix='/api/v1')
 
 
 @app.route('/')
 def my_todos():
     return render_template('index.html')
+
+
+@app.route('/api/v1/users/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,8 +1,61 @@
 import datetime
 
+from argon2 import PasswordHasher
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature, SignatureExpired)
+
 from peewee import *
+import config
 
 db = SqliteDatabase('todo.sqlite')
+HASHER = PasswordHasher()
+
+
+class User(Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = db
+
+    @classmethod
+    def create_user(cls, username, email, password, **kwargs):
+        email = email.lower()
+        try:
+            cls.select().where(
+                (cls.email == email) | (cls.username ** username)
+            ).get()
+        except cls.DoesNotExist:
+            user = cls(username=username, email=email)
+            user.password = user.set_password(password)
+            user.save()
+            return user
+        else:
+            raise Exception("User with that username or email already exists.")
+
+    @staticmethod
+    def verify_auth_token(token):
+        serializer = Serializer(config.SECRET_KEY)
+        try:
+            data = serializer.loads(token)
+        except (SignatureExpired, BadSignature):
+            return None
+        else:
+            user = User.get(User.id == data['id'])
+            return user
+
+    @staticmethod
+    def set_password(password):
+        return HASHER.hash(password)
+
+    def verify_password(self, password):
+        return HASHER.verify(self.password, password)
+
+    def generate_auth_token(self, expires=360000):
+        serializer = Serializer(config.SECRET_KEY, expires_in=expires)
+        return serializer.dumps({'id': self.id})
+
 
 class Todo(Model):
     """Hold all the To-do tasks."""
@@ -11,6 +64,7 @@ class Todo(Model):
     completed = BooleanField(default=False)
     created_at = DateTimeField(default=datetime.datetime.now)
     updated_at = DateTimeField(null=True, default=None)
+    user = ForeignKeyField(User, related_name='todo_set', on_delete='CASCADE')
 
     class Meta:
         database = db
@@ -18,5 +72,5 @@ class Todo(Model):
 
 def initialize():
     db.connect(reuse_if_open=True)
-    db.create_tables([Todo], safe=True)
+    db.create_tables([User, Todo], safe=True)
     db.close()
